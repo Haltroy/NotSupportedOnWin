f6047815c341a87a20140e9fb4bc7747dc10b920
haltroy <thehaltroy@gmail.com>
2023-05-13 20:26:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace NotSupportedOnWin;
+
+public static class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32S:
+                case PlatformID.Win32NT:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                case PlatformID.Xbox:
+                    throw new PlatformNotSupportedException("Windows is not supported right now.");
+                // TODO: Add Windows Support
+                // jk
+
+                default:
+                case PlatformID.Unix:
+                case PlatformID.MacOSX:
+                case PlatformID.Other:
+                    Console.WriteLine($"hello {Environment.OSVersion.VersionString}");
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error while running application, exception caught: {e.ToString()}");
+        }
+    }
+}
